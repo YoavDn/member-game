6e824a024fa547c5adf886431fd9df12c588e2d8
yoav <yoavdan.m@gmail.com>
2022-08-07 13:05:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
+import PetList from './PetList';
+import PetForm from './petForm';
 import './App.css';
+
+
+const pets = ['Joey', 'Look', "Jeny"]
+// const petList = pets.map(pet => <div>{pet}</div>)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PetForm />
     </div>
+
   );
 }
 
